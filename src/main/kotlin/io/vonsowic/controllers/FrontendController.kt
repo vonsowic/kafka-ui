1d89d78dabb8f0243c057c825e75dda47eb45b52
Michał Wąsowicz <mwasowicz7@gmail.com>
2022-10-06 19:58:02 +0200

    server-side routing
    Signed-off-by: Michał Wąsowicz <mwasowicz7@gmail.com>

--- src/main/kotlin/io/vonsowic/controllers/FrontendController.kt
+++ src/main/kotlin/io/vonsowic/controllers/FrontendController.kt
@@ -1,17 +1,25 @@
 package io.vonsowic.controllers
 
+import io.micronaut.context.annotation.Value
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
+import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
-import java.net.URI
+import io.micronaut.http.annotation.Produces
+import java.nio.file.Files
+import java.nio.file.Path
 
 
 @Controller("/")
-class FrontendController {
+class FrontendController(
+    @Value("\${kafka-ui.ui-dir}")
+    private val indexHtmlFilePath: String
+) {
 
+    @Produces(MediaType.TEXT_HTML)
     @io.micronaut.http.annotation.Error(status = HttpStatus.NOT_FOUND, global = true)
     fun forwardToFrontend(request: HttpRequest<Any>): HttpResponse<out Any> {
-       return HttpResponse.redirect(URI("/"))
+        return HttpResponse.ok(Files.readString(Path.of(indexHtmlFilePath, "index.html")))
     }
 }
