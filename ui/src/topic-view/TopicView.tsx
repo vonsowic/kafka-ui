42505e8f8f3c45fc2fdce37306fcf43ea06fccc5
Michał Wąsowicz <mwasowicz7@gmail.com>
2022-10-23 13:08:53 +0200

    add search input to filter matching events
    Signed-off-by: Michał Wąsowicz <mwasowicz7@gmail.com>

--- ui/src/topic-view/TopicView.tsx
+++ ui/src/topic-view/TopicView.tsx
@@ -1,7 +1,7 @@
 import { useCallback, useEffect, useRef, useState } from "react";
 import Axios from 'axios'
 import { useParams } from "react-router-dom";
-import { Button, Card, Dimmer, Icon, Loader, Pagination, Statistic } from "semantic-ui-react";
+import { Button, Card, Dimmer, Form, Icon, Input, Loader, Pagination, Statistic } from "semantic-ui-react";
 import { KafkaEvent, Topic, TopicPartition } from "../dto";
 import { JSONView } from "./JsonView";
 
@@ -59,16 +59,53 @@ function fetchEvents(topic: Topic, numberOfPages: number, page: number): Promise
   Math.max(
     partition.earliestOffset,
     partition.latestOffset - (numberOfPages - page - Number(isLatest) + 1) * NUM_OF_EVENTS_PER_PAGE
-)
-const params = topic.partitions
-  .map(partition => ({ 
-    [`t0p${partition.id}e`]: offsetRange(partition, false),
-    [`t0p${partition.id}l`]: offsetRange(partition, true)
-  }))
-  .reduce((acc: any, it: any) => ({ ...acc, ...it }), ({ t0: topic.name } as any))
+  )
+  const params = topic.partitions
+    .map(partition => ({ 
+      [`t0p${partition.id}e`]: offsetRange(partition, false),
+      [`t0p${partition.id}l`]: offsetRange(partition, true)
+    }))
+    .reduce((acc: any, it: any) => ({ ...acc, ...it }), ({ t0: topic.name } as any))
 
-return Axios.get<KafkaEvent[]>('/api/events', { params })
-  .then(({ data }) => data)}
+  return Axios.get<KafkaEvent[]>('/api/events', { params })
+    .then(({ data }) => data)
+}
+
+
+function sseUrl(topic: Topic, events: PartitionedEvents): string {
+  const params = 
+    Object.entries(events)
+      .map(([partition, partitionEvents]) => ({ partition, offset: 1 + Math.max(...partitionEvents.map(event => event.offset)) }))
+      .reduce((acc, it) => ({...acc, [`t0p${it.partition}e`]: it.offset }), { t0: topic.name } )
+
+  return sseUrlByParams(params)
+}
+
+function searchUrl(topic: Topic, search: string): string {
+  const params = 
+    topic.partitions
+      .reduce((acc, partition) => ({
+        ...acc,
+        [`t0p${partition.id}e`]: partition.earliestOffset,
+        [`t0p${partition.id}l`]: partition.latestOffset,
+      }), { 
+        t0: topic.name,
+        s: search
+      })
+
+  return sseUrlByParams(params)
+}
+
+function sseUrlByParams(params: { [key: string]: string}): string {
+  const paramStr =
+    Object.entries(params)
+      .map(([key, value]) => `${key}=${value}`)
+      .join('&')
+
+  return process.env.NODE_ENV === "development"
+    ? `http://0.0.0.0:8080/api/events?${paramStr}`
+    : `/api/events?${paramStr}`
+}
 
 function TopicView() {
   const { topicName } = useParams();
@@ -80,13 +117,17 @@ function TopicView() {
   const [isStreaming, setStreaming] = useState(false)
   const [loadingMessage, setLoadingMessage] = useState<string | null>(null)
   const [events, setEvents] = useState({} as PartitionedEvents) // events per partition
-  const [topic, setTopic] = useState(undefined as any as Topic)
+  const [topic, setTopic] = useState<Topic | undefined>(undefined)
+  
+  const [search, setSearch] = useState('')
+  const [searchFormValue, setSearchFormValue] = useState('')
+  
   const addKafkaEvent = useCallback(
     (kafkaEvent: any) => {
       const event = JSON.parse(kafkaEvent) as KafkaEvent
       const { partition } = event
       const offset = event.offset + 1
-      const topicPartition = topic.partitions.find(p => p.id === partition)
+      const topicPartition = topic?.partitions.find(p => p.id === partition)
       if (topicPartition?.latestOffset && offset > topicPartition.latestOffset) {
         topicPartition.latestOffset = offset
         setTopic(topic)
@@ -103,6 +144,9 @@ function TopicView() {
   ) 
 
   const loadEvents = (page: number) => {
+    if (!topic) {
+      throw new Error('cannot load events without topic metadata')
+    }
     setLoadingMessage('Loading events...')
     const numOfPages = numberOfPages(topic)
     fetchEvents(topic, numOfPages, page)
@@ -125,38 +169,43 @@ function TopicView() {
       return
     }
 
-    if (isStreaming && !eventSource.current) {
-      const params = 
-        Object.entries(events)
-          .map(([partition, partitionEvents]) => ({ partition, offset: 1 + Math.max(...partitionEvents.map(event => event.offset)) }))
-          .reduce((acc, it) => ({...acc, [`t0p${it.partition}e`]: it.offset }), { t0: topicName } )
-
-      console.log('params', params)
-      const paramStr =
-          Object.entries(params)
-            .map(([key, value]) => `${key}=${value}`)
-            .join('&')
-
-      const url = process.env.NODE_ENV === "development"
-        ? `http://0.0.0.0:8080/api/events?${paramStr}`
-        : `/api/events?${paramStr}`
-
-      eventSource.current = new EventSource(url) 
+    if (search && !eventSource.current) {
+      console.log('searching')
+      eventSource.current = new EventSource(searchUrl(topic, search)) 
       eventSource.current.onmessage = (message) => addKafkaEvent(message.data)
-      return () => {}
+      eventSource.current.onerror= (err) => {
+        console.log('xxx', err)
+        setSearch('')
+      }
+      return () => {
+        console.log('searching exit')
+      }
+    }  
+
+    if (isStreaming && !eventSource.current) {
+      eventSource.current = new EventSource(sseUrl(topic, events)) 
+      eventSource.current.onmessage = (message) => addKafkaEvent(message.data)
+      return () => {
+        console.log('sse exit')
+      }
     } 
     
     if (eventSource.current) {
       eventSource.current?.close()
       eventSource.current = null
-      return () => {}
+      return () => {
+        console.log('closing sse exit')
+      }
     }  
     
     loadEvents(numberOfPages(topic))
-    return () => {}
+    return () => {
+      console.log('fetch events exit')
+    }
   // eslint-disable-next-line
-  }, [topicName, topic, isStreaming])
+  }, [topicName, topic, isStreaming, search])
 
+  console.log('refreshing view')
   return (
     <>
       <Dimmer active={loadingMessage !== null} inverted>
@@ -169,16 +218,44 @@ function TopicView() {
       </Statistic>
 
       <Pagination
-          disabled={isStreaming}
+          disabled={isStreaming || search !== ''}
           totalPages={numberOfPages(topic)} 
           activePage={page(topic, events)}
           onPageChange={(_, changedPage) => {
             loadEvents(Number(changedPage.activePage))
           }}
       />
-      <Button icon basic onClick={() => setStreaming(!isStreaming)}>
+      <Button icon basic onClick={() => setStreaming(!isStreaming)} disabled={search !== ''}>
         <Icon name={isStreaming ? 'pause' : 'play'} />
       </Button>
+
+      <Form
+        onSubmit={() => {
+          if (!searchFormValue) {
+            return
+          }
+
+          setEvents([])
+          setSearch(searchFormValue)
+        }}
+          style={{ display: 'inline-block', width: '24rem'}} >
+        <Input
+          disabled={isStreaming}
+          icon='search'
+          loading={search !== ''}
+          placeholder='Search...'  
+          onChange={e => {
+            setSearchFormValue(e.currentTarget.value)
+            if (!e.currentTarget.value) {
+              getTopic(topicName)
+                .then(topic => {
+                  setLoadingMessage(null)
+                  setTopic(topic)
+                })
+            }
+          }} 
+         />
+      </Form>
       
       <Card.Group>
         {
